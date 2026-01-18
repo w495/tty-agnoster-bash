9ed5aae7585ca2ac92a1b3e37ffa125bd94eb9b2
w-495 <w-495@mail.ru>
2026-01-18 23:06:07 +0300

    reduce sub-shells

--- lib/parts/vcs/git.bash
+++ lib/parts/vcs/git.bash
@@ -3,29 +3,21 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../segment.bash"
 
-__tty_ag_git_status_dirty() {
-  local dirty
-  dirty=$(git status -s 2> /dev/null | tail -n 1 || true)
-  if [[ -n ${dirty} ]]; then
-    echo '[!]'
-  fi
-}
-
-__tty_ag_git_stash_dirty() {
-  local stash
-  stash=$(git stash list 2> /dev/null | tail -n 1 || true)
-  if [[ -n ${stash} ]]; then
-    echo '[F]'
-  fi
-}
 
 # Git: branch/detached head, dirty status
 __tty_ag_prompt_git() {
   local ref dirty line
   if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     # ZSH_THEME_GIT_PROMPT_DIRTY='±'
-    dirty=$(__tty_ag_git_status_dirty)
-    stash=$(__tty_ag_git_stash_dirty)
+    dirty=$(git status -s 2> /dev/null | tail -n 1 || true)
+    if [[ -n ${dirty} ]]; then
+      dirty='[D]'
+    fi
+    stash=$(git stash list 2> /dev/null | tail -n 1 || true)
+    if [[ -n ${stash} ]]; then
+      stash='[S]'
+    fi
+
     ref=$(git symbolic-ref HEAD 2> /dev/null)
     if [[ -z "${ref}" ]]; then
      ref="- $(git describe --exact-match --tags HEAD 2> /dev/null)"
@@ -33,11 +25,11 @@ __tty_ag_prompt_git() {
     if [[ -z "${ref}" ]]; then
      ref="- $(git show-ref --head -s --abbrev | head -n1 2> /dev/null || true)"
     fi
-    line="${ref/refs\/heads\//│╯ }${stash}${dirty}"
+    line="${ref/refs\/heads\//Y }${stash}"
     if [[ -n ${dirty} ]]; then
-      __tty_ag_prompt_segment_left yellow black "${line} (o_O)"
+      __tty_ag_prompt_segment_left yellow black "(o_O) ${line}"
     else
-      __tty_ag_prompt_segment_left green black "${line} (^_^)"
+      __tty_ag_prompt_segment_left green black "(^_^) ${line}"
     fi
 
   fi
